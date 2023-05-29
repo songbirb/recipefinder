77cb77fb4705f06e1dc64174b63270dd140efa32
songbirb <arif.huenerli05@gmail.com>
2023-05-29 19:56:54 +0200

    recipecontroller test method added (it works=)

--- src/main/java/com/recipefinder/recipe/boundary/RecipeController.java
+++ src/main/java/com/recipefinder/recipe/boundary/RecipeController.java
@@ -1,4 +1,15 @@
 package com.recipefinder.recipe.boundary;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recipe")
 public class RecipeController {
+
+    @GetMapping
+    public String test() {
+        return "hello";
+    }
 }
